dda87b2e2ad6c29c81c551d165d40896b2dad26f
sirhamsteralot <sirhamsteralot@gmail.com>
2018-07-17 00:56:12 +0200

    allow two modifiers

--- HaEPluginCore/HaEInputHandler_Sub.cs
+++ HaEPluginCore/HaEInputHandler_Sub.cs
@@ -13,14 +13,27 @@ namespace HaEPluginCore
         {
             private readonly MyKeys _key;
             private readonly MyKeys _modifier;
+            private readonly MyKeys _modifier2;
 
             private readonly Action _callback;
 
+            public HaEKeyCombination(MyKeys key, MyKeys modifier, MyKeys modifier2, Action callback)
+            {
+                _key = key;
+                _modifier = modifier;
+                _modifier2 = modifier2;
+
+                _callback = callback;
+            }
 
             public void CheckCombinationPressed()
             {
-                if (MyInput.Static.IsKeyPress(_key) && MyInput.Static.IsKeyPress(_modifier))
+                if (MyInput.Static.IsKeyPress(_key) && 
+                   (MyInput.Static.IsKeyPress(_modifier) || _modifier == MyKeys.None) && 
+                   (MyInput.Static.IsKeyPress(_modifier2) || _modifier2 == MyKeys.None))
+                {
                     _callback?.Invoke();
+                }
             }
         }
     }
