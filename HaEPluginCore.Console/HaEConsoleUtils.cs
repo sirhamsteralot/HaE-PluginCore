55e83e72070f783fa408109569e06350a26db047
sirhamsteralot <sirhamsteralot@gmail.com>
2018-10-17 23:23:29 +0200

    Add game references

--- HaEPluginCore.Console/HaEConsoleUtils.cs
+++ HaEPluginCore.Console/HaEConsoleUtils.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Scripting;
+using SpaceEngineers.Game;
 
 namespace HaEPluginCore.Console
 {
@@ -40,8 +41,8 @@ namespace HaEPluginCore.Console
             {
                 var op = ScriptOptions.Default
                                       .WithReferences("System.Runtime, Version = 4.0.0.0, Culture = neutral, PublicKeyToken = b03f5f7f11d50a3a")
-                                      .WithReferences(typeof(List<>).Assembly, typeof(Enumerable).Assembly, typeof(string).Assembly, typeof(HaEPluginCore).Assembly, typeof(StringBuilder).Assembly)
-                                      .WithImports("System", "System.Collections.Generic", "System.Timers", "System.Linq", "System.Text", "HaEPluginCore");
+                                      .WithReferences(typeof(List<>).Assembly, typeof(Enumerable).Assembly, typeof(string).Assembly, typeof(HaEPluginCore).Assembly, typeof(StringBuilder).Assembly, typeof(SpaceEngineersGame).Assembly)
+                                      .WithImports ("System", "System.Collections.Generic", "System.Timers", "System.Linq", "System.Text", "HaEPluginCore");
 
                 var res = await CSharpScript.EvaluateAsync<object>(code, op);
                 GC.Collect();
