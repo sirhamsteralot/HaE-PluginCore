c19d3f8178b37d9791fe614b312ef298e98d1847
sirhamsteralot <sirhamsteralot@gmail.com>
2018-10-18 02:06:39 +0200

    Networking stuff YOINKED from torch

---
+++ HaEPluginCore/Reflection.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaEPluginCore
+{
+    public static class Reflection
+    {
+
+        public static bool HasMethod(Type type, string methodName, Type[] argTypes = null)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(methodName))
+                    return false;
+
+                if (argTypes == null)
+                {
+                    var methodInfo = type.GetMethod(methodName);
+                    if (methodInfo == null)
+                        methodInfo = type.GetMethod(methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+                    if (methodInfo == null && type.BaseType != null)
+                        methodInfo = type.BaseType.GetMethod(methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+                    if (methodInfo == null)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    MethodInfo method = type.GetMethod(methodName, argTypes);
+                    if (method == null)
+                        method = type.GetMethod(methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy, Type.DefaultBinder, argTypes, null);
+                    if (method == null && type.BaseType != null)
+                        method = type.BaseType.GetMethod(methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy, Type.DefaultBinder, argTypes, null);
+                    if (method == null)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            catch (AmbiguousMatchException)
+            {
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static bool HasField(Type type, string fieldName)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(fieldName))
+                    return false;
+                var field = type.GetField(fieldName);
+                if (field == null)
+                    field = type.GetField(fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+                if (field == null)
+                    field = type.BaseType.GetField(fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+                if (field == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static bool HasProperty(Type type, string propertyName)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(propertyName))
+                    return false;
+                var prop = type.GetProperty(propertyName);
+                if (prop == null)
+                    prop = type.GetProperty(propertyName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+                if (prop == null)
+                    prop = type.BaseType?.GetProperty(propertyName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+                if (prop == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Invokes the static method of the given type, with the given arguments.
+        /// </summary>
+        /// <param name="type">Type the method is contained in</param>
+        /// <param name="methodName">Method name</param>
+        /// <param name="args">Arguments to the method</param>
+        /// <returns>return value of the invoked method, or null if it failed</returns>
+        public static object InvokeStaticMethod(Type type, string methodName, params object[] args)
+        {
+            var method = type.GetMethod(methodName, BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            if (method == null)
+            {
+                return null;
+            }
+
+            return method.Invoke(null, args);
+        }
+
+        /// <summary>
+        /// Invokes the private method with the given arguments on the instance.  Includes base types of instance.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <param name="methodName"></param>
+        /// <param name="args"></param>
+        /// <returns>the return value of the method, or null if it failed</returns>
+        public static object InvokePrivateMethod(object instance, string methodName, params object[] args)
+        {
+            Type type = instance.GetType();
+            while (type != null)
+            {
+                MethodInfo method = type.GetMethod(methodName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                if (method != null)
+                    return method.Invoke(instance, args);
+                type = type.BaseType;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the value of a private field in an instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the private field</typeparam>
+        /// <param name="obj">The instance</param>
+        /// <param name="fieldName">Field name</param>
+        /// <param name="recurse">Should the base types be examined</param>
+        /// <returns></returns>
+        public static T GetPrivateField<T>(this object obj, string fieldName, bool recurse = false)
+        {
+            var type = obj.GetType();
+            while (type != null)
+            {
+                FieldInfo field = type.GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+                if (field != null)
+                    return (T)field
+                                  .GetValue(obj);
+
+                if (!recurse)
+                    break;
+                type = type.BaseType;
+            }
+            return default(T);
+        }
+
+        /// <summary>
+        /// Gets the list of all delegates registered in the named static event
+        /// </summary>
+        /// <param name="type">The type (or child type) that contains the event</param>
+        /// <param name="eventName">Name of the event</param>
+        /// <returns>All delegates registered with the event</returns>
+        public static IEnumerable<Delegate> GetStaticEvent(Type type, string eventName)
+        {
+            return GetEventsInternal(null, eventName, type);
+        }
+
+        /// <summary>
+        /// Gets the list of all delegates registered in the named event
+        /// </summary>
+        /// <param name="instance">Instance to retrieve the event list for</param>
+        /// <param name="eventName">Name of the event</param>
+        /// <returns>All delegates registered with the event</returns>
+        public static IEnumerable<Delegate> GetInstanceEvent(object instance, string eventName)
+        {
+            return GetEventsInternal(instance, eventName);
+        }
+
+        private static readonly string[] _backingFieldForEvent = { "{0}", "<backing_store>{0}" };
+        private static IEnumerable<Delegate> GetEventsInternal(object instance, string eventName, Type baseType = null)
+        {
+            BindingFlags bindingFlags = BindingFlags.NonPublic |
+                               (instance == null ? BindingFlags.Static : BindingFlags.Instance);
+
+            FieldInfo eventField = null;
+            baseType = baseType ?? instance?.GetType();
+            Type type = baseType;
+            while (type != null && eventField == null)
+            {
+                for (var i = 0; i < _backingFieldForEvent.Length && eventField == null; i++)
+                    eventField = type.GetField(string.Format(_backingFieldForEvent[i], eventName), bindingFlags);
+                type = type.BaseType;
+            }
+            if (eventField?.GetValue(instance) is MulticastDelegate eventDel)
+            {
+                foreach (Delegate handle in eventDel.GetInvocationList())
+                    yield return handle;
+            }
+
+        }
+    }
+}
