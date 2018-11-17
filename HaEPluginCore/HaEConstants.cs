e62742d6f3fe258aaf953076fffe22459608eba5
sirhamsteralot <sirhamsteralot@gmail.com>
2018-11-17 17:02:05 +0100

    Some additions to the plugin constants

--- HaEPluginCore/HaEConstants.cs
+++ HaEPluginCore/HaEConstants.cs
@@ -9,10 +9,13 @@ namespace HaEPluginCore
 {
     public static class HaEConstants
     {
+        public const int versionNumber = 1040;  //1.0.4.0
+
         public const float screenScaleConstant = 1.33333337f;
         public static string pluginFolder => Path.GetDirectoryName(typeof(HaEConstants).Assembly.Location);
         public const string StorageFolder = "Config";
         public const string ScriptFolder = "Scripts";
+        public const string AssetFolder = "Assets";
 
         public static TimeSpan quarterSecTimeOut = TimeSpan.FromMilliseconds(250);
     }
