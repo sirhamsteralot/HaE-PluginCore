9718620d6d4968258a456817b9851fed13151f3d
SirHamsterAlot <SirHamsterAlot@gmail.com>
2018-11-21 09:39:11 +0100

    this shouldnt be a const xocS

--- HaEPluginCore/HaEConstants.cs
+++ HaEPluginCore/HaEConstants.cs
@@ -9,7 +9,7 @@ namespace HaEPluginCore
 {
     public static class HaEConstants
     {
-        public const int versionNumber = 1041;  //1.0.4.1
+        public static int versionNumber = 1041;  //1.0.4.1
 
         public const float screenScaleConstant = 1.33333337f;
         public static string pluginFolder => Path.GetDirectoryName(typeof(HaEConstants).Assembly.Location);
