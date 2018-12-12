a40b4f869bc9bb4a62982a35c91deb8edf35d010
sirhamsteralot <sirhamsteralot@gmail.com>
2018-12-12 02:21:03 +0100

    Switch to harmony

--- HaEPluginCore/HaEPatcher.cs
+++ HaEPluginCore/HaEPatcher.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using Harmony;
 
 namespace HaEPluginCore
 {
@@ -12,27 +13,8 @@ namespace HaEPluginCore
     {
         public static void ReplaceMethod(MethodInfo methodToReplace, MethodInfo methodToInject)
         {
-            RuntimeHelpers.PrepareMethod(methodToReplace.MethodHandle);
-            RuntimeHelpers.PrepareMethod(methodToInject.MethodHandle);
-
-            unsafe
-            {
-                if (IntPtr.Size == 4)
-                {
-                    int* inj = (int*)methodToInject.MethodHandle.Value.ToPointer() + 2;
-                    int* tar = (int*)methodToReplace.MethodHandle.Value.ToPointer() + 2;
-
-                    *tar = *inj;
-                }
-                else
-                {
-
-                    long* inj = (long*)methodToInject.MethodHandle.Value.ToPointer() + 1;
-                    long* tar = (long*)methodToReplace.MethodHandle.Value.ToPointer() + 1;
-
-                    *tar = *inj;
-                }
-            }
+            var harmony = HarmonyInstance.Create("com.HaE.HaECore.PluginCore");
+            harmony.Patch(methodToReplace, new HarmonyMethod(methodToInject));
         }
     }
 }
