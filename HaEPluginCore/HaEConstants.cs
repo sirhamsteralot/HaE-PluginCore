91370d711ab28cd4efad0d416a2aab3ea715b0d6
sirhamsteralot <sirhamsteralot@gmail.com>
2018-12-15 17:50:32 +0100

    VersionNumber

--- HaEPluginCore/HaEConstants.cs
+++ HaEPluginCore/HaEConstants.cs
@@ -9,7 +9,7 @@ namespace HaEPluginCore
 {
     public static class HaEConstants
     {
-        public static int versionNumber = 1050;  //1.0.5.0
+        public static int versionNumber = 1060;  //1.0.6.0
 
         public const float screenScaleConstant = 1.33333337f;
         public static string pluginFolder => Path.GetDirectoryName(typeof(HaEConstants).Assembly.Location);
